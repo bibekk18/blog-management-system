6303493f3a0eb7318aca5ce36d9cb6d2d584d7bc
bibek18 <you@example.com>
2025-03-25 09:36:14 +0545

    learned about deleting single items in crud operation

--- src/Pages/blog/SingleBlock.jsx
+++ src/Pages/blog/SingleBlock.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/layout/Layout'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios'
+import { baseurl } from '../../config'
 
 const SingleBlock = () => {
+
+   const {id} = useParams()
+const [blog,setBlog]=useState({})
+const navigate = useNavigate()
+
+
+   const deleteBlog =async()=>{
+    const response = await axios.delete(`${baseurl}/blog/${id}`,{
+
+    headers:{
+        'Authorization':localStorage.getItem('token')
+    }
+    })
+    if(response.status ===200){
+     navigate('/')
+    }
+     else{
+        alert("something went wrong")
+     }
+    
+}
+   const fetchBlog =async()=>{
+    const response = await axios.get(`${baseurl}/blog/${id}`)
+    if(response.status ===200){
+     setBlog(response.data.data)
+    }
+    
+   }
+   useEffect (()=>{
+     fetchBlog()
+   },[])
+   
   return (
     
 
@@ -13,7 +47,7 @@ const SingleBlock = () => {
         <div className="flex flex-col md:flex-row -mx-4">
             <div className="md:flex-1 px-4">
                 <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
-                    <img className="w-full h-full object-cover" src="https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206355_960_720.jpg" alt="Product Image"/>
+                    <img className="w-full h-full object-cover" src={blog?.imageUrl} alt="Product Image"/>
                 </div>
                 <div className="flex -mx-2 mb-4">
                     <div className="w-1/2 px-2">
@@ -22,36 +56,30 @@ const SingleBlock = () => {
                   </Link>
                     </div>
                     <div className="w-1/2 px-2">
-                        <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Delete</button>
+                        <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600" onClick ={deleteBlog} >Delete</button>
                     </div>
                 </div>
             </div>
             <div className="md:flex-1 px-4">
-                <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">Blog title</h2>
+                <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">{blog?.title}</h2>
                 <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-                    ante justo. Integer euismod libero id mauris malesuada tincidunt.
+                    {blog.subtitle}
                 </p>
                 <div className="flex mb-4">
                     <div className="mr-4">
-                        <span className="font-bold text-gray-700 dark:text-gray-300">Category</span>
-                        <span className="text-gray-600 dark:text-gray-300">$29.99</span>
+                        <span className="font-bold text-gray-700 dark:text-gray-300">Category:</span>
+                        <span className="text-gray-600 dark:text-gray-300">${blog.category}</span>
                     </div>
                     <br />
                     <div>
-                        <span className="font-bold text-gray-700 dark:text-gray-300">Published at:</span>
-                        <span className="text-gray-600 dark:text-gray-300">In Stock</span>
+                        <span className="font-bold text-gray-700 dark:text-gray-300">Author:{blog?.userId?.username}</span>
                     </div>
                 </div>
                 
                 <div>
                     <span className="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
                     <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                        sed ante justo. Integer euismod libero id mauris malesuada tincidunt. Vivamus commodo nulla ut
-                        lorem rhoncus aliquet. Duis dapibus augue vel ipsum pretium, et venenatis sem blandit. Quisque
-                        ut erat vitae nisi ultrices placerat non eget velit. Integer ornare mi sed ipsum lacinia, non
-                        sagittis mauris blandit. Morbi fermentum libero vel nisl suscipit, nec tincidunt mi consectetur.
+                        {blog.description}
                     </p>
                 </div>
             </div>
